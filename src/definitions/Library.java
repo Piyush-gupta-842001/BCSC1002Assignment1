db5450d911df3a4beed0a51bf16aeb65560c91c1
PIYUSH KUMAR GUPTA <piyush.gupta_cs19@gla.ac.in>
2020-08-30 23:37:14 +0530

    create getter and setter method for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,7 +7,12 @@ package definitions;
 public class Library {
     private Book[] availableBooks;
 
+    public Book[] getAvailableBooks () {
+        return availableBooks;
+    }
 
-
+    public void setAvailableBooks ( Book[] availableBooks ) {
+        this.availableBooks = availableBooks;
+    }
 }
 
