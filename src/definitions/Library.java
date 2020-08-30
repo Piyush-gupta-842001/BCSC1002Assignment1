e75596292c013fb61a642fbd0ad3949c0daeddcb
PIYUSH KUMAR GUPTA <piyush.gupta_cs19@gla.ac.in>
2020-08-30 23:32:34 +0530

    create private array field for Library.java class.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -4,41 +4,10 @@
  * */
 package definitions;
 
-import java.util.Arrays;
-
 public class Library {
     private Book[] availableBooks;
 
-    public Library ( Book[] availableBooks ) {
-        this.availableBooks = availableBooks;
-    }
 
-    public Book[] getAvailableBooks () {
-        return availableBooks;
-    }
 
-    public void setAvailableBooks ( Book[] availableBooks ) {
-        this.availableBooks = availableBooks;
-    }
-
-    @Override
-    public String toString () {
-        return "Library{" +
-                "availableBooks=" + Arrays.toString ( availableBooks ) +
-                '}';
-    }
-
-    @Override
-    public boolean equals ( Object o ) {
-        if (this == o) return true;
-        if (o == null || getClass () != o.getClass ()) return false;
-        Library library = (Library) o;
-        return Arrays.equals ( getAvailableBooks (), library.getAvailableBooks () );
-    }
-
-    @Override
-    public int hashCode () {
-        return Arrays.hashCode ( getAvailableBooks () );
-    }
 }
 
