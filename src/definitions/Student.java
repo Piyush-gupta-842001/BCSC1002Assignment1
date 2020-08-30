6b8b4e6d2fe78ab04040189813f82ac154fc776a
PIYUSH KUMAR GUPTA <piyush.gupta_cs19@gla.ac.in>
2020-08-30 23:27:25 +0530

    create getter and setter method in Student.java class.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -19,5 +19,61 @@ public class Student {
         this.studentUniversityRollNumber = studentUniversityRollNumber;
     }
 
+    public String getStudentFirstName () {
+        return studentFirstName;
+    }
+
+    public void setStudentFirstName ( String studentFirstName ) {
+        this.studentFirstName = studentFirstName;
+    }
+
+    public String getStudentMiddleName () {
+        return studentMiddleName;
+    }
+
+    public void setStudentMiddleName ( String studentMiddleName ) {
+        this.studentMiddleName = studentMiddleName;
+    }
+
+    public String getStudentLastName () {
+        return studentLastName;
+    }
+
+    public void setStudentLastName ( String studentLastName ) {
+        this.studentLastName = studentLastName;
+    }
+
+    public String getStudentFullName () {
+        return studentFullName;
+    }
+
+    public void setStudentFullName ( String studentFullName ) {
+        this.studentFullName = studentFullName;
+    }
+
+    public long getStudentUniversityRollNumber () {
+        return studentUniversityRollNumber;
+    }
+
+    public void setStudentUniversityRollNumber ( long studentUniversityRollNumber ) {
+        this.studentUniversityRollNumber = studentUniversityRollNumber;
+    }
+
+    public int getNumberOfBooksIssuedByTheStudent () {
+        return numberOfBooksIssuedByTheStudent;
+    }
+
+    public void setNumberOfBooksIssuedByTheStudent ( int numberOfBooksIssuedByTheStudent ) {
+        this.numberOfBooksIssuedByTheStudent = numberOfBooksIssuedByTheStudent;
+    }
+
+    public Book[] getNameOfAllTheBooksIssuedByTheStudent () {
+        return nameOfAllTheBooksIssuedByTheStudent;
+    }
+
+    public void setNameOfAllTheBooksIssuedByTheStudent ( Book[] nameOfAllTheBooksIssuedByTheStudent ) {
+        this.nameOfAllTheBooksIssuedByTheStudent = nameOfAllTheBooksIssuedByTheStudent;
+    }
+
 
 }
