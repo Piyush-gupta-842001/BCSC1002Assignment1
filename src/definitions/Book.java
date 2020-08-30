f9295bd230fe5c9f379fd0933a4d71d0eec6bfb3
PIYUSH KUMAR GUPTA <piyush.gupta_cs19@gla.ac.in>
2020-08-30 22:59:54 +0530

    create getter and setter method for student class.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -16,5 +16,29 @@ public class Book {
         this.bookISBNNumber = bookISBNNumber;
     }
 
+    public String getBookName () {
+        return bookName;
+    }
+
+    public void setBookName ( String bookName ) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName () {
+        return authorName;
+    }
+
+    public void setAuthorName ( String authorName ) {
+        this.authorName = authorName;
+    }
+
+    public long getBookISBNNumber () {
+        return bookISBNNumber;
+    }
+
+    public void setBookISBNNumber ( long bookISBNNumber ) {
+        this.bookISBNNumber = bookISBNNumber;
+    }
+
 
 }
